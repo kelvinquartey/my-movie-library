eb48d57547d1c74fb32030d587ea5f3cc0ec3726
kelvinquartey <kelvinquartey1@gmail.com>
2025-07-15 17:17:22 +0000

    First Commit - Home Page

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+    )}`
+);
+    const data = await response.json()
+    return data.results
+};
