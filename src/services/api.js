330589543c5e2d9f550e6e1a232b002f3b6ef04e
kelvinquartey <kelvinquartey1@gmail.com>
2025-09-03 18:49:44 +0000

    api.js changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+// const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+// const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
+
+const API_KEY = "5d1e8f0b6b954b62afc3ed323c47d35a";
+const BASE_URL= "https://api.themoviedb.org/3"
 
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
